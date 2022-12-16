3a81376b3b2ae03fdbe87b84ecc748d57b1ec77e
James Brown <jamesbrown19921992@hotmail.com>
2022-12-16 13:59:20 +0000

    handling add and remove

--- src/components/SetSelection.js
+++ src/components/SetSelection.js
@@ -1,17 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 export default function SetSelection() {
+
+    const [setCounter, setSetCounter] = useState([1]);
+    const [rowCount, setRowCount] = useState(1)
+
+    const handleAddSubmit = event => {
+        event.preventDefault();
+        setSetCounter(setCounter => [...setCounter, setCounter.length + 1])
+        setRowCount(rowCount + 1);
+    }
+
+    const handleRemoveSubmit = event => {
+        event.preventDefault();
+        setCounter.splice(setCounter.length-1, 1)
+        setSetCounter([...setCounter])
+    }
+    
     return (
         <div>
-            <div>SetSelection</div>
+            <div>Select Reps and Weight:</div>
             <form>
                 <label>
-                    Name:
-                    <input type="text" name="name" />
+                    <div>
+                        {setCounter.map((value) => (
+                            <div>
+                                {console.log('setCounter = ' + setCounter)}
+                                {console.log('value = ' + value)}
+
+                                <input type="text" name="reps" placeholder='Enter Reps' />
+                                <input type="text" name="weight" placeholder='Enter Weight' />
+                                {value == setCounter.length &&
+                                    <button onClick={(e) => handleAddSubmit(e)}>Add</button>
+                                }
+                                {(value > 1 && value == setCounter.length) &&
+                                    <button onClick={(e) => handleRemoveSubmit(e)}>Remove</button>
+                                }
+                            </div>
+                        ))}
+                    </div>
                 </label>
-                <input type="submit" value="Submit" />
             </form>
         </div>
     )
